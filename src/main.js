86789e4a7c214ad6072035b6a04ec146674e8b58
Thomas Demkey <thomasd@criticalmass.com>
2020-04-23 09:46:55 -0600

    adding current time display for cities

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import { cities as _cities } from "./navigation.json";
+import { cities as _cities } from "./data/navigation.json";
+import { timezones as _timezones } from "./data/timezones.json";
 
-const navItemOnClick = (selected) => {
-  const selectedElement = document.getElementsByClassName(selected)[0];
-  selectedElement.classList.add("selected");
+/**
+ * When the Nav Item is clicked we will set the selected classname onto it.
+ */
+const setSelectedState = (selected) => {
+  if (selected) {
+    const selectedElement = document.getElementsByClassName(selected)[0];
+    selectedElement.classList.add("selected");
+  }
 };
 
+/**
+ * Here we need to remove the selected state from any other divs that may contain import PropTypes from 'prop-types'
+ * otherwise we end up with multiple selected cities
+ */
 const removeSelectedState = () => {
   const navItems = document.querySelectorAll(".city-wrapper");
 
@@ -13,7 +23,12 @@ const removeSelectedState = () => {
   });
 };
 
-const setSelectedBarWidth = (selectedEl) => {
+/**
+ * In order to properly set the width of our sliding bar element to to be the size of our label
+ * we need to get the label size and offset then set them in our element. This setting of the offset
+ * along with the css transition is what gives us our "sliding" effect.
+ */
+const setSelectedBarWidthOffset = (selectedEl) => {
   const selectBar = document.getElementById("selected-bar");
   const selectedElement = document.getElementsByClassName(selectedEl)[0]
     .firstElementChild;
@@ -25,15 +40,60 @@ const setSelectedBarWidth = (selectedEl) => {
   selectBar.style.left = `${elementOffset}px`;
 };
 
-const selectCity = (selectedCity) => {
-  removeSelectedState();
-  navItemOnClick(selectedCity);
-  setSelectedBarWidth(selectedCity);
+const getUTCTime = async () => {
+  const response = await fetch("http://worldclockapi.com/api/json/utc/now");
+  const data = await response.json();
+  return data;
 };
 
+const getTimeForCity = async (selectedCity) => {
+  if (_timezones[selectedCity]) {
+    const now = new Date();
+    const userOffset = now.getTimezoneOffset();
+    const cityOffset = _timezones[selectedCity];
+
+    const userHourOffset = Math.floor(userOffset / 60);
+    const userMinuteOffset = 60 * (userOffset % 60);
+
+    const totalHourOffset = cityOffset.hour + userHourOffset;
+    const totalMinuteOffset = cityOffset.minute + userMinuteOffset;
+
+    const currentHours = now.getHours() + totalHourOffset;
+    const currentMinutes = now.getMinutes() + totalMinuteOffset;
+
+    const amPm = currentHours < 12 || currentHours > 24 ? "am" : "pm";
+
+    let displayHours = currentHours;
+    if (currentHours > 12) {
+      displayHours = currentHours - 12;
+    }
+    if (currentHours > 24) {
+      displayHours = currentHours - 24;
+    }
+
+    console.log(currentHours);
+    console.log(currentMinutes);
+    document.getElementById("hours").innerHTML = displayHours;
+    document.getElementById("minutes").innerHTML = currentMinutes;
+    document.getElementById("am-pm").innerHTML = amPm;
+  }
+};
+
+/**
+ * Gathering function to pull in the different parts of what happens when a city is selected
+ */
+const selectCity = (selectedCity) => {
+  removeSelectedState();
+  setSelectedState(selectedCity);
+  setSelectedBarWidthOffset(selectedCity);
+};
+
+/**
+ * The main entry function for creating and managing our Navigation Component
+ */
 const createNavBar = () => {
   const navigationEl = document.getElementById("navigation");
-  _cities.forEach((city, index) => {
+  _cities.forEach((city) => {
     const cityWrapper = document.createElement("div");
     cityWrapper.classList.add(city.section);
     cityWrapper.classList.add("city-wrapper");
@@ -45,15 +105,17 @@ const createNavBar = () => {
     cityWrapper.append(navLabel);
     cityWrapper.tabIndex = 1;
 
-    cityWrapper.onclick = () => {
+    cityWrapper.onclick = async () => {
       selectCity(city.section);
+      await getTimeForCity(city.section);
     };
 
     navigationEl.append(cityWrapper);
   });
 
   selectCity(_cities[0].section);
-  window.onresize = () => setSelectedBarWidth("selected");
+  window.onresize = () => setSelectedBarWidthOffset("selected");
 };
 
+//Initializing the Navigation on loading the page
 createNavBar();
