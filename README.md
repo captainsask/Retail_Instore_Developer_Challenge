53bd1c2e539576c5a25499abe2e0ab6c6f928c6b
Thomas Demkey <thomasd@criticalmass.com>
2020-04-23 10:12:53 -0600

    Updating documentation on code and readme updating time display for minutes

--- README.md
+++ README.md
@@ -1,30 +1,20 @@
-# rollup-starter-app
+# Navigation Developer challenge
+
+This repo utilizes the rollup-starter-app boilerplate repository for bundling and deploying the application to a Node server. See below for commands to run the application.
+
+This application creates a simple navigation bar using Vanilla JS, CSS and HTML and will display the timezone for the selected city below the nav
 
 This repo contains a bare-bones example of how to create an application using Rollup, including importing a module from `node_modules` and converting it from CommonJS.
 
-*See also https://github.com/rollup/rollup-starter-lib*
-
+_See also https://github.com/rollup/rollup-starter-lib_
 
 ## Getting started
 
 Clone this repository and install its dependencies:
 
-```bash
-git clone https://github.com/rollup/rollup-starter-app
-cd rollup-starter-app
-npm install
-
-# or
-npx degit "rollup/rollup-starter-app" my-app
-cd my-app
-npm install
-```
-
-The `public/index.html` file contains a `<script src='bundle.js'>` tag, which means we need to create `public/bundle.js`. The `rollup.config.js` file tells Rollup how to create this bundle, starting with `src/main.js` and including all its dependencies, including [date-fns](https://date-fns.org).
-
 `npm run build` builds the application to `public/bundle.js`, along with a sourcemap file for debugging.
 
-`npm start` launches a server, using [serve](https://github.com/zeit/serve). Navigate to [localhost:3000](http://localhost:3000).
+`npm start` launches a server, using [serve](https://github.com/zeit/serve). Navigate to [localhost:5000](http://localhost:5000).
 
 `npm run watch` will continually rebuild the application as your source files change.
 
--- src/main.js
+++ src/main.js
@@ -40,14 +40,21 @@ const setSelectedBarWidthOffset = (selectedEl) => {
   selectBar.style.left = `${elementOffset}px`;
 };
 
-const getUTCTime = async () => {
-  const response = await fetch("http://worldclockapi.com/api/json/utc/now");
-  const data = await response.json();
-  return data;
-};
+/**
+ * To avoid using libraries or making outside calls we will use the timezone offset
+ * from the users system combined with the timezone offset from the timezones json
+ * to calculate the time in the specific city,
+ *
+ * Ideally we would be able to pull in timezones from another api however for the exercise
+ * I have used a user maintained JSON file
+ */
 
 const getTimeForCity = async (selectedCity) => {
+  // if the user has not update the timezones for a city we won't display the clock
+  document.getElementById("clock-wrapper").style.display = "none";
+  //If there is a timezone entry for that city though we will do our calculations to dispaly the local time
   if (_timezones[selectedCity]) {
+    document.getElementById("clock-wrapper").style.display = "flex";
     const now = new Date();
     const userOffset = now.getTimezoneOffset();
     const cityOffset = _timezones[selectedCity];
@@ -70,11 +77,11 @@ const getTimeForCity = async (selectedCity) => {
     if (currentHours > 24) {
       displayHours = currentHours - 24;
     }
+    const displayMinutes =
+      currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes;
 
-    console.log(currentHours);
-    console.log(currentMinutes);
     document.getElementById("hours").innerHTML = displayHours;
-    document.getElementById("minutes").innerHTML = currentMinutes;
+    document.getElementById("minutes").innerHTML = displayMinutes;
     document.getElementById("am-pm").innerHTML = amPm;
   }
 };
@@ -86,12 +93,13 @@ const selectCity = (selectedCity) => {
   removeSelectedState();
   setSelectedState(selectedCity);
   setSelectedBarWidthOffset(selectedCity);
+  getTimeForCity(selectedCity);
 };
 
 /**
  * The main entry function for creating and managing our Navigation Component
  */
-const createNavBar = () => {
+const init = () => {
   const navigationEl = document.getElementById("navigation");
   _cities.forEach((city) => {
     const cityWrapper = document.createElement("div");
@@ -118,4 +126,4 @@ const createNavBar = () => {
 };
 
 //Initializing the Navigation on loading the page
-createNavBar();
+init();
