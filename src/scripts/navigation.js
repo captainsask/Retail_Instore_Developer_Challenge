4421052c7489866d4b4e07960e0bd8614ff1e1f3
Thomas Demkey <thomasd@criticalmass.com>
2020-04-22 19:41:03 -0600

    Commiting initial work on navigation challenge to repository. Code works on code pen currently, will continue to connect.

---
+++ src/scripts/navigation.js
@@ -0,0 +1,56 @@
+import navigation from "../data/navigation.json";
+
+const navItemOnClick = (selected) => {
+  const selectedElement = document.getElementsByClassName(selected)[0];
+  selectedElement.classList.add("selected");
+};
+
+const removeSelectedState = () => {
+  const navItems = document.querySelectorAll(".city-wrapper");
+
+  navItems.forEach((item) => {
+    item.classList.remove("selected");
+  });
+};
+
+const setSelectedBarWidth = (selectedEl) => {
+  const selectBar = document.getElementById("selected-bar");
+  const selectedElement = document.getElementsByClassName(selectedEl)[0]
+    .firstElementChild;
+
+  const selectedWidth = selectedElement.clientWidth;
+  const elementOffset = selectedElement.offsetLeft;
+
+  selectBar.style.width = `${selectedWidth}px`;
+  selectBar.style.left = `${elementOffset}px`;
+};
+
+const selectCity = (selectedCity) => {
+  removeSelectedState();
+  navItemOnClick(selectedCity);
+  setSelectedBarWidth(selectedCity);
+};
+
+(function createNavBar() {
+  const navigationEl = document.getElementById("navigation");
+  navigation.cities.forEach((city, index) => {
+    const cityWrapper = document.createElement("div");
+    cityWrapper.classList.add(city.section);
+    cityWrapper.classList.add("city-wrapper");
+
+    const navLabel = document.createElement("p");
+    navLabel.innerHTML = city.label;
+    navLabel.classList.add("nav-item");
+
+    cityWrapper.append(navLabel);
+    cityWrapper.tabIndex = 1;
+
+    cityWrapper.onclick = () => {
+      selectCity(city.section);
+    };
+
+    navigationEl.append(cityWrapper);
+  });
+
+  selectCity(navigation.cities[0].section);
+})();
